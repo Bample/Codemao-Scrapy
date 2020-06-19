0f68fa7de205711258f4de9c5876537e183b1c49
zzy2357 <zzy2357@outlook.com>
2020-06-19 20:17:57 +0800

    build: Build Tool

---
+++ build.sh
@@ -0,0 +1,15 @@
+# Windows
+
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ./dist/Codemao-Scrapy-win64.exe
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./dist/Codemao-Scrapy-win32.exe
+
+# Mac OS
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./dist/Codemao-Scrapy-mac
+
+# Linux
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./dist/Codemao-Scrapy-linux
+
+echo "Done!"
